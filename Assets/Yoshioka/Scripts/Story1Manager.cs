45aac6ae3d20f6309b50c86096aff99ff9e4c294
Hal-0823 <haru20040823@gmail.com>
2023-09-17 08:26:02 +0900

    雑魚敵イカの追加
    プレイヤーに狙いを定めて突進してくるイカを追加しました。

---
+++ Assets/Yoshioka/Scripts/Story1Manager.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using UnityEngine.SceneManagement;
+
+public class Story1Manager : MonoBehaviour
+{
+    public CameraScroll cameraScroll;
+    public PlayerController playerController;
+    public GameEvent BeforeStageStart;
+    public GameObject playerInfo;   //UI
+    public CanvasGroup blackScreen;  //暗転用
+    public DialogueManager dialogueManager;
+    private Color screenColor;
+
+    void Start()
+    {
+        //blackScreenを透明にする
+        blackScreen.alpha = 0;
+        //Cameraのスクロールを止める
+        cameraScroll.PouseCameraScroll(true);
+
+        //Playerの操作を不可にする
+        playerController.StopControll(true);
+
+        playerInfo.SetActive(false);
+
+        //Dialogueパート開始
+        dialogueManager.StartDialogue(0);
+
+        //Dialogueパートが終了時に呼び出される
+        dialogueManager.OnEndLog = () =>{
+            StartCoroutine(PhaseA());
+        };
+    }
+
+    IEnumerator PhaseA()
+    {
+        yield return new WaitForSeconds(0.6f);
+
+        AudioManager.Instance.PlayBGM_FromIntroToLoop("BGM調査頭","BGM調査ループ");
+
+        //BeforeStageStartをRistenしているものを実行する
+        BeforeStageStart.Raise();
+
+        yield return new WaitForSeconds(3.8f);
+
+        StartStage();
+    }
+
+    public void StartStage()
+    {
+        cameraScroll.PouseCameraScroll(false);
+        cameraScroll.StartCoroutine("FadeIn",1.5f);
+        playerController.StopControll(false);
+        playerInfo.SetActive(true);
+    }
+     
+    public void Boss()
+    {
+        playerController.StopControll(true);
+       
+        cameraScroll.PouseCameraScroll(true);
+
+        dialogueManager.StartDialogue(1);
+
+        dialogueManager.OnEndLog = () =>{
+            StartCoroutine(PhaseB());
+        };
+    }
+
+    IEnumerator PhaseB()
+    {
+        yield return new WaitForSeconds(0.6f);
+    }
+
+    public void GameOver()
+    {        
+        cameraScroll.StartCoroutine("FadeOut");
+
+        AudioManager.Instance.PlayBGM("BGMゲームオーバー",0.5f);
+
+        blackScreen.DOFade(0.8f,2.0f);
+    }
+
+    public void Click_Retry()
+    {
+        LoadingManager.Instance.LoadScene("Stage0",2.0f);
+    }
+
+    public void Click_Back()
+    {
+        LoadingManager.Instance.LoadScene("Title",2.0f);
+    }
+
+
+    IEnumerator BlackOut()
+    {
+
+        yield return new WaitForSeconds(1.0f);
+        SceneManager.LoadScene("Title");
+    }
+}
